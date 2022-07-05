3ee658ad29afe38231acddaa7e990c58c7d89401
chloelauu <mabinogigames7@gmail.com>
2022-07-05 12:14:23 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <meta name="description" content="This is a heroku app.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Heroku App</title>
 </head>
